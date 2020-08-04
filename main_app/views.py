dcd9942e67c74e0e6a0b20a12cf19143f64e98ae
jimmyspann <jimmyleespann@outlook.com>
2020-08-04 13:39:45 -0400

    added templates with data examples

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse, redirect
 
+class Data:
+  def __init__(self, test):
+    self.test = test
+
+
+data = Data("TESTING DATA")
+
 # Create your views here.
+
 def home(request):
-  return HttpResponse("IT WORKS!")
+    context = {
+        'data': data
+    }
+    return render(request, 'home.html', context)
