ed6d82c439f37eaf5c3159db48b5a57d256a907e
jimmyspann <jimmyleespann@outlook.com>
2020-08-05 17:17:30 -0400

    created post.html and modified models

--- main_app/models.py
+++ main_app/models.py
@@ -7,21 +7,28 @@ from django.contrib.auth.models import User
 #Profiles
 class Profile(models.Model):
     city = models.CharField(max_length=100)
-    img = models.CharField(max_length=300, blank=True, null=True)
-    # user = models.CharField(max_length=100)
-
-
-#Posts
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.CharField(max_length=100000)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # city = models.CharField(max_length=100)
+    img = models.CharField(max_length=300, default="images/default_profile.png")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+          return self.user.username
 
 #Cities
 class City(models.Model):
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    img = models.CharField(max_length=300)
-    # posts = models.CharField(max_length=100)
-    
+    img = models.CharField(max_length=300, default="images/default_city.png")
+
+    def __str__(self):
+        return self.name    
+
+#Posts
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=100000)
+    img = models.CharField(max_length=300, default="images/default_post.png")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+      return self.title
