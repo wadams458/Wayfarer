b6cc1039728eeb6d090cfa0d9b1a613fe17230e6
wadams <wwadams94@gmail.com>
2020-08-07 16:06:43 -0700

    updated updated landing page

--- main_app/views.py
+++ main_app/views.py
@@ -15,6 +15,10 @@ def home(request):
     # return HttpResponse(f"{posts[0]} {posts[1]}")
     return render(request, 'home.html', context)
 
+def about(request):
+    
+    # return HttpResponse(f"{posts[0]} {posts[1]}")
+    return render(request, 'about.html')
 
 # ------------------- CITIES -------------------
 
