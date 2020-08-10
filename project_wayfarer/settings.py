daa2b703b47c2dbd4e757004736761dab4bc0d14
jimmyspann <jimmyleespann@outlook.com>
2020-08-10 13:15:58 -0400

    fixed cities background

--- project_wayfarer/settings.py
+++ project_wayfarer/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'wayfarer',
-        # 'USER': 'Jimmy',
-        # 'PASSWORD': 'test'
+        'USER': 'Jimmy',
+        'PASSWORD': 'test'
     }
 }
 
